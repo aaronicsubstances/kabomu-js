b7edddb8d1b1e0196e27000f056c9ed158d57524
Aaron Baffour-Awuah <aaronbaffourawuah@gmail.com>
2023-08-19 09:24:02 +0000

    Tested ContentLengthEnforcingCustomReader module.

--- src/common/ContentLengthEnforcingCustomReader.ts
+++ src/common/ContentLengthEnforcingCustomReader.ts
@@ -5,7 +5,10 @@ import { CustomIOError } from "./errors";
 const generate = async function*(wrappedReader: Readable, expectedLength: bigint) {
     let bytesLeftToRead = expectedLength;
     if (!bytesLeftToRead) {
-        return;
+        // if bytes left to read is zero,
+        // leave it to Readable.from() result to decide
+        // on whether or not to go ahead and call backing reader
+        // (e.g. so that any error in backing reader can be thrown).
     }
     for await (const chunk of wrappedReader) {
         if (bytesLeftToRead < 0) {
@@ -13,8 +16,8 @@ const generate = async function*(wrappedReader: Readable, expectedLength: bigint
         }
         else if (bytesLeftToRead >= chunk.length) {
             yield chunk;
-            bytesLeftToRead -= chunk.length;
-            if (bytesLeftToRead === BigInt(0)) {
+            bytesLeftToRead -= BigInt(chunk.length);
+            if (!bytesLeftToRead) {
                 break;
             }
         }
@@ -27,13 +30,26 @@ const generate = async function*(wrappedReader: Readable, expectedLength: bigint
             break;
         }
     }
+    // if end of read is encountered, ensure that all
+    // requested bytes have been read.
     if (bytesLeftToRead > 0) {
         throw CustomIOError.createContentLengthNotSatisfiedError(
                 expectedLength, bytesLeftToRead);
     }
 }
 
+/**
+ * Wraps another readable stream to ensure a given amount of bytes are read.
+ * @param wrappedReader the backing reader
+ * @param expectedLength the expected number of bytes to guarantee or assert.
+ * Can be negative to indicate that the all remaining bytes in the backing reader
+ * should be returned
+ * @returns a stream decorating the reader argument
+ */
 export function createContentLengthEnforcingCustomReader(
-        wrappedReader: Readable, expectedLength: bigint) {
-    return Readable.from(generate(wrappedReader, expectedLength));
+        wrappedReader: Readable, expectedLength: bigint | number) {
+    if (!wrappedReader) {
+        throw new Error("wrappedReader argument is null");
+    }
+    return Readable.from(generate(wrappedReader, BigInt(expectedLength)));
 }
